72626ce03c073de255df06b64145602406f4f57c
Marwin Dollendorf <marwin.dollendorf@ruhr-uni-bochum.de>
2022-04-16 19:26:56 +0200

    Added simple player object, a very simple world and basic movement script.

---
+++ Assets/Scrips/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    [SerializeField] float speed = 8f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVector.x = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVector.y = 1;
+        }
+        transform.position += moveVector.normalized * Time.deltaTime * speed;
+    }
+}
